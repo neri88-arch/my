64aebc005f34b78bb51b1493cf90a8d1ea323498
neri88-arch <arzianijustin@gmail.com>
2026-07-26 19:14:43 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ set -e
 
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S --needed --noconfirm base-devel distrobox virt-manager qemu podman tor zsh ttf-meslo-nerd git fastfetch sl devtools neovim cuda kitty qwen-code unzip
+sudo pacman -S --needed --noconfirm base-devel distrobox virt-manager qemu podman tor zsh ttf-meslo-nerd git fastfetch sl devtools neovim cuda kitty qwen-code unzip polkit-kde-agent
 
 git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si --noconfirm && cd .. && rm -rf paru
 
