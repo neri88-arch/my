ebacb2bf15ba817aa0b0340438188b7c7d4709a8
neri88-arch <arzianijustin@gmail.com>
2026-08-08 07:05:22 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,13 @@ set -e
 
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S --needed --noconfirm base-devel distrobox virt-manager qemu podman tor zsh ttf-meslo-nerd git cmake fastfetch sl devtools neovim cuda kitty unzip polkit-kde-agent qemu-desktop virt-viewer uv bat eza zoxide gwenview mpv
+sudo pacman -S --needed --noconfirm base-devel distrobox virt-manager qemu podman tor zsh ttf-meslo-nerd git cmake fastfetch sl devtools neovim cuda kitty unzip polkit-kde-agent qemu-desktop virt-viewer uv bat eza zoxide gwenview mpv btop
 
 git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si --noconfirm && cd .. && rm -rf paru
 
 paru -S --needed --noconfirm obfs4proxy zen-browser zsh-theme-powerlevel10k-git qutebrowser sddm-conf noctalia-shell oh-my-pi-bin sddm-theme-tokyo-night-git
 
-sudo pacman -Rsn vim vim-runtime alacritty waybar
+sudo pacman -Rsn vim vim-runtime alacritty waybar top 
 
 CHSH=no RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
